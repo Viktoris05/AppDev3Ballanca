d88092335bec619e4b143069a1c245735aa4d8f7
martinpirker <martin.pirker00@gmx.at>
2025-07-20 22:22:59 +0200

    Added Music

--- Ballance/app/src/main/java/com/example/ballance/MainActivity.kt
+++ Ballance/app/src/main/java/com/example/ballance/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.ballance
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,13 +9,38 @@ import com.example.ballance.ui.navigation.AppNavHost
 import com.example.ballance.ui.theme.BallanceTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Musik starten
+        mediaPlayer = MediaPlayer.create(this, R.raw.music)
+        mediaPlayer?.isLooping = true
+        mediaPlayer?.start()
+
         setContent {
             BallanceTheme {
                 val navController = rememberNavController()
                 AppNavHost(navController)
             }
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer?.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlayer?.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
